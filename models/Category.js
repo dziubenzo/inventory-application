332f98b1ae8854f2051a7a1ab1f9c9566d3b301b
dziubenzo <dziubenzo@gmail.com>
2024-03-12 11:26:11 +0100

    Create Category schema and model

---
+++ models/Category.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+const CategorySchema = new Schema({
+  name: { type: String, required: true, maxLength: 100 },
+  description: { type: String, required: true, maxLength: 100 },
+  slug: { type: String, required: true, maxLength: 100 },
+});
+
+// Virtual for category's URL
+CategorySchema.virtual('url').get(function () {
+  return `/categories/${this.slug}`;
+});
+
+module.exports = mongoose.model('Category', CategorySchema);
