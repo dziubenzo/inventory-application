9b89b82ad57864235330043ebec0cbe4ec99b7be
dziubenzo <dziubenzo@gmail.com>
2024-03-12 18:35:25 +0100

    Build route handler for '/categories'

--- controllers/categoryController.js
+++ controllers/categoryController.js
@@ -1,8 +1,18 @@
+const Category = require('../models/Category');
 const Item = require('../models/Item');
 const asyncHandler = require('express-async-handler');
 
 exports.all_categories = asyncHandler(async (req, res, next) => {
-  res.send('All categories');
+  // Get all categories from DB
+  const allCategories = await Category.find({}).sort({ name: 1 }).exec();
+  // Add count of items to each category
+  for (const category of allCategories) {
+    category.itemCount = await Item.countDocuments({
+      category: category._id,
+    }).exec();
+  }
+
+  res.render('all_categories', { title: 'Categories', allCategories });
 });
 
 exports.create_category = asyncHandler(async (req, res, next) => {
