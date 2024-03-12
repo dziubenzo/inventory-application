3df64e015635c4ba63098549c3074789f68752a4
dziubenzo <dziubenzo@gmail.com>
2024-03-12 16:40:51 +0100

    Build route handler for '/items'

--- controllers/itemController.js
+++ controllers/itemController.js
@@ -2,7 +2,13 @@ const Item = require('../models/Item');
 const asyncHandler = require('express-async-handler');
 
 exports.all_items = asyncHandler(async (req, res, next) => {
-  res.send('All items');
+  // Get all items from DB
+  const allItems = await Item.find({}, 'name category')
+    .populate('category')
+    .sort({ name: 1 })
+    .exec();
+
+  res.render('all_items', { title: 'Items', allItems });
 });
 
 exports.create_item = asyncHandler(async (req, res, next) => {
