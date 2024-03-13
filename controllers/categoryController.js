c8ede93f966e65b9d4ec928d703d885ae8fb9b48
dziubenzo <dziubenzo@gmail.com>
2024-03-13 14:04:21 +0100

    Build POST route handler for '/categories/create'

--- controllers/categoryController.js
+++ controllers/categoryController.js
@@ -1,6 +1,9 @@
 const Category = require('../models/Category');
 const Item = require('../models/Item');
+
 const asyncHandler = require('express-async-handler');
+const { body, validationResult } = require('express-validator');
+const slugify = require('slugify');
 
 exports.all_categories = asyncHandler(async (req, res, next) => {
   // Get all categories from DB
@@ -16,12 +19,66 @@ exports.all_categories = asyncHandler(async (req, res, next) => {
 });
 
 exports.create_category_get = asyncHandler(async (req, res, next) => {
-  res.render('create_category', {title: 'Create Category'})
+  res.render('create_category', { title: 'Create Category' });
 });
 
-exports.create_category_post = asyncHandler(async (req, res, next) => {
-  res.send('Create category');
-});
+exports.create_category_post = [
+  // Validate and sanitise both category fields
+  body('name', 'Category name must contain between 3 and 100 characters.')
+    .trim()
+    .isLength({ min: 3, max: 100 })
+    .escape(),
+
+  body(
+    'description',
+    'Category description must contain between 3 and 400 characters.'
+  )
+    .trim()
+    .isLength({ min: 3, max: 400 })
+    .escape(),
+
+  asyncHandler(async (req, res, next) => {
+    // Extract the validation errors from a request.
+    const errors = validationResult(req);
+
+    const name = req.body.name;
+    const description = req.body.description;
+
+    // Create category with the name field turned into a slug
+    const category = new Category({
+      name: name,
+      description: description,
+      slug: slugify(name, { lower: true }),
+    });
+
+    // Render the page again with sanitised values and error messages if there are errors.
+    if (!errors.isEmpty()) {
+      res.render('create_category', {
+        title: 'Create Category',
+        category,
+        errors: errors.array(),
+      });
+    } else {
+      // Check if the category with the same name already exists.
+      const categoryExists = await Category.findOne({ name: name }).exec();
+
+      // Render the form again with relevant error message
+      if (categoryExists) {
+        // Create custom error array for use in the template
+        const errorArray = [{ msg: 'Category already exists.' }];
+        res.render('create_category', {
+          title: 'Create Category',
+          category,
+          errors: errorArray,
+        });
+      } else {
+        // Save category and redirect to category details
+        await category.save();
+        res.redirect(category.url);
+      }
+    }
+  }),
+];
 
 exports.category_details = asyncHandler(async (req, res, next) => {
   // Get requested category from DB based on URL parameter
--- models/Category.js
+++ models/Category.js
@@ -3,8 +3,14 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const CategorySchema = new Schema({
-  name: { type: String, required: true, maxLength: 100, unique: true },
-  description: { type: String, required: true, maxLength: 100 },
+  name: {
+    type: String,
+    required: true,
+    minLength: 3,
+    maxLength: 100,
+    unique: true,
+  },
+  description: { type: String, required: true, minLength: 3, maxLength: 400 },
   slug: {
     type: String,
     required: true,
