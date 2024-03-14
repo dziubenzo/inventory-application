c2a34b5ec018b3884ca827815ea9a891d6a677da
dziubenzo <dziubenzo@gmail.com>
2024-03-14 18:31:24 +0100

    Prepare for deployment

--- app.js
+++ app.js
@@ -8,15 +8,24 @@ const indexRouter = require('./routes/index');
 const itemRouter = require('./routes/item');
 const categoryRouter = require('./routes/category');
 
+const compression = require('compression');
+const helmet = require('helmet');
+
 const app = express();
+
+// Rate limiter - 30 requests per minute
+const RateLimit = require('express-rate-limit');
+const limiter = RateLimit({
+  windowMs: 1 * 60 * 1000,
+  max: 30,
+});
 
 // MongoDB connection
 const mongoose = require('mongoose');
-const mongoURL = require('./mongoURL');
 
 mongoose.set('strictQuery', false);
 
-const mongoDB = mongoURL || process.env.MONGODB_URI;
+const mongoDB = process.env.MONGODB_URI;
 
 main().catch((err) => console.log(err));
 
@@ -32,6 +41,9 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(compression());
+app.use(helmet());
+app.use(limiter);
 app.use(express.static(path.join(__dirname, 'public')));
 
 // Routes
